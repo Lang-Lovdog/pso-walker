bbf7eb4c9b52f5397427ea5e3d4960ac17da2dc3
Lang-Lovdog <brandon.mozart@gmail.com>
2023-05-12 18:31:28 -0600

    Ultimos detalles d'a carpeta compartir

--- COMPARTIR_T3.c/FuncionObjetivo.f90
+++ COMPARTIR_T3.c/FuncionObjetivo.f90
@@ -6,24 +6,11 @@ module pso_funcion_objetivo
 contains
    subroutine FuncionObjetivoF(VDP, CDV, RDO) bind(C, name="FuncionObjetivoF")
       INTEGER(C_INT), INTENT(IN), VALUE :: CDV
-      !INTEGER(C_INT), INTENT(IN), VALUE :: CDP
       REAL(C_FLOAT), INTENT(INOUT) :: VDP(CDV)
-      !REAL(C_FLOAT), INTENT(INOUT) :: PDO(CDP)
       REAL(C_FLOAT), INTENT(INOUT) :: RDO(1)
       INTEGER :: k
-      !REAL :: H(5)
       ! Result variable initialization
       RDO(1) = 0
-      ! H(1) = 1
-      ! H(2) = 2
-      ! H(3) = 3
-      ! H(4) = 4
-      ! H(5) = 5
-      !WRITE (*, *) VDP(:)
-      !WRITE (*, *) SUM(ABS(VDP(:)))
-      !WRITE (*, *) PRODUCT(VDP(:))
-      !WRITE (*, *) SUM(ABS(VDP(:)))
-      !PRINT *, -(SUM(ABS(VDP(:))) + PRODUCT(VDP(:)))
 
   !!!!! Mathematical Functions for PSO Evaluation !!!!!
     !!! F2
@@ -39,7 +26,7 @@ contains
       !   RDO = RDO + ((100*((VDP(k + 1) - VDP(k)**2)**2)) + ((VDP(k) - 1)**2))
       !end do
     !!! F6
-      RDO(1) = SUM((floor(VDP + 1.0/2)))
+      !RDO(1) = SUM((floor(VDP + 1.0/2)))
     !!! F7
       !
     !!! F8
